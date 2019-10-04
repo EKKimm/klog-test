14904dddf77614e18343a020ce4781c04fbe2847
yonghui yu <dydgml1935@gmail.com>
2019-10-04 20:12:02 +0900

    Revert "controller 완성"
    This reverts commit 79ab8c4a4955af8f3cd570066cf7454b853c0065.

--- controller/userController.js
+++
@@ -1,4 +0,0 @@
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("User Detail");
-export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
